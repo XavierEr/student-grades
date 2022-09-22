6fae0c65258efbaadebd836a55ad36428a37f0f9
Xavier Er <xavier_er@live.com.sg>
2022-09-23 01:05:20 +0800

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+//#region Library imports
+import React, { StrictMode } from 'react';
+//#endregion
+
+//#region React Component imports
+//#endregion
+
+
+const App: React.FC = (): JSX.Element => {
+  return (
+    <StrictMode>
+    </StrictMode>
+  );
+}
+
+export default App;
