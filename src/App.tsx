e6e8c2cc69915836f3ab788e53f60c4fcd985939
Xavier Er <xavier_er@live.com.sg>
2022-09-23 01:11:59 +0800

    Import GradeTracker and make it responsive

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import React, { StrictMode } from 'react';
 //#endregion
 
 //#region React Component imports
+import GradeTracker from './components/GradeTracker';
 //#endregion
 
+import styles from './styles/components/App.module.scss';
 
 const App: React.FC = (): JSX.Element => {
   return (
     <StrictMode>
+      <div className={styles.container}>
+        <div className={styles.header}>Students</div>
+        
+        <GradeTracker className={styles.gradeTracker} />
+      </div>
     </StrictMode>
   );
 }
