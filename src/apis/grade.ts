2165c93d622143702a255d09e9089bba65968adc
Xavier Er <xavier_er@live.com.sg>
2022-09-24 00:42:46 +0800

    Add getGradesWithRetries function

--- src/apis/grade.ts
+++ src/apis/grade.ts
@@ -6,14 +6,31 @@ import axios from 'axios';
 import type { CancelToken } from 'axios';
 //#endregion
 
-export async function getGrades(cancelToken: CancelToken): Promise<Array<Grade>> {
-
+export async function getGrades(cancelToken?: CancelToken): Promise<Array<Grade>> {
   const url = 'https://quanmgx57hjiqicdxgo2vzebqq0tghim.lambda-url.ap-southeast-1.on.aws/';
-  
-  const response = await axios.get<Array<Grade>>(url, { cancelToken });
+
+  const response = await axios.get<Array<Grade>>(url, { timeout: 3000, cancelToken });
 
   if (response.status === 200) {
     return response.data;
   }
   throw new Error(response.statusText);
+}
+
+export async function getGradesWithRetries(retries: number, cancelToken?: CancelToken): Promise<Array<Grade>> {
+  try {
+    const grades = await getGrades(cancelToken);
+
+    return grades;
+  } catch (error: any) {
+    if (error.message !== 'component_unmount') {
+      const retriesLeft = --retries;
+
+      if (retriesLeft !== 0) {
+        return getGradesWithRetries(retriesLeft, cancelToken);
+      }
+      throw error;
+    }
+    return [];
+  }
 }
